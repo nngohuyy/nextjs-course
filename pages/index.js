8d4a7fe3d47c563b8b71146ec3f92fd972f28c2b
Nguyen Ngo Huy <ngohuy200338@gmail.com>
2022-10-24 17:39:47 +0700

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,45 @@
-import { useState } from 'react';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-function Header({ title }) {
-	return <h1>{title ? title : 'Default title'}</h1>;
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
 }
-
-export default function HomePage() {
-	const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-	const [likes, setLikes] = useState(0);
-
-	function handleClick() {
-		setLikes(likes + 1);
-	}
-
+export default function Home({ allPostsData }) {
 	return (
-		<div>
-			<Header title="Developed. Previewed. Shipped. 🚀" />
-			<ul>
-				{names.map((name) => (
-					<li key={name}>{name}</li>
-				))}
-			</ul>
-
-			<button onClick={handleClick}>Like ({likes})</button>
-		</div>
-	);
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>Hello from the other side.</p>
+				<p>I must have called a thousand times</p>
+				<p>To tell you I'm sorry for everything that I've done</p>
+				<p>
+					(This is a sample website - you'll be building a site like this in
+					<a href="https://nextjs.org/learn"> our Next.js tutortial</a>.)
+				</p>
+			</section>
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	)
 }
